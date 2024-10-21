6b021180f2b7c786d641e8d3bd24fbfbbaa5ec5b
Goodmorningpeople <zachy2020p@gmail.com>
2024-10-21 08:56:20 +0800

    Initial Commit

---
+++ DrakeDev_Incoin/DrakeDev_IncoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrakeDev_IncoinApp.swift
+//  DrakeDev_Incoin
+//
+//  Created by Ihateapple on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct DrakeDev_IncoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DrakeDev_IncoinTests/DrakeDev_IncoinTests.swift
@@ -0,0 +1,16 @@
+//
+//  DrakeDev_IncoinTests.swift
+//  DrakeDev_IncoinTests
+//
+//  Created by Ihateapple on 21/10/24.
+//
+
+import Testing
+
+struct DrakeDev_IncoinTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
